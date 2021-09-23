674b2b64b9f33eab4a2769974029439fffc51fde
Brian Merrick <brianmerrick@boomi.com>
2021-09-23 07:48:36 -0600

    first commit

---
+++ src/Boomi/ExternalFunctions.java
@@ -0,0 +1,13 @@
+package Boomi;
+
+import java.nio.charset.Charset;
+import java.util.Random;
+
+public class ExternalFunctions {
+	
+	public static String RandomString() {
+		byte[] array = new byte[7]; // length is bounded by 7
+	    new Random().nextBytes(array);
+	    return new String(array, Charset.forName("UTF-8"));
+	}
+}
